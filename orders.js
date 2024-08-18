9cb70c3541f0c461cf91b2fc60e53b55f48f5d0c
flavio-ph <flaviophalmeida@gmail.com>
2024-08-18 19:29:21 -0300

    Dashboard

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Devendo',
+        status: 'Pendente'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'pago',
+        status: 'Efetivado'
+    },
+]
